e97b1ba0a7501016ef9bf19692b7c08aadef9718
pintjuk <danielpintjuk@gmail.com>
2013-10-07 20:24:48 +0200

    better folder structure

---
+++ Protocol/protocol.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hej")
+
+}
+
+func sendInt64(int64 message)
+func sendASCII(string message)
+
+func sendAddUpdate(string add, language lang)
+func ~
+
+finc
---
+++ Server/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func constructQuery() (query func(net.Conn), rconsol <-chan string) {
+	consol := make(chan string, 5)
+	rconsol = consol
+	query = func(conn net.Conn) {
+		consol <- "hej"
+		conn.Write(byte[](":P"))
+		conn.Close()
+		close(consol)
+	}
+	return
+}
+
+func main() {
+	query, consol := constructQuery()
+	go server(query, ":9001")
+
+	for i := range consol {
+		fmt.Println(i)
+	}
+}
